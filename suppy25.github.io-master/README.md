f4329b24f513fdbd56e064feff6bc84f345f9793
Suprava Sahoo <61075828+suppy25@users.noreply.github.com>
2020-09-22 22:53:46 +0530

    Add files via upload

---
+++ suppy25.github.io-master/README.md
@@ -0,0 +1,4 @@
+# My Portfolio on GitHub Pages
+My portfolio designed using HTML, CSS, and JavaScript on GitHub Pages to highlight my projects, and my work.
+
+Explore more about me: https://www.Supravasahoo.com/
---
+++ suppy25.github.io-master/Scripts/updates_data_file.js
@@ -0,0 +1,30 @@
+latest_updates = [
+	{
+		"content" : "Mercedes-Benz-Greener-Manufacturing",
+		"url": "https://github.com/suppy25/Mercedes-Benz-Greener-Manufacturing",
+		"date": "July 22, 2020"
+	},
+	{
+		"content" : "Text-similarity-in-fake-news-classification-in-COVID-19-domain",
+		"url": "https://github.com/suppy25/Text-similarity-in-fake-news-classification-in-COVID-19-domain",
+		"date": "July 20, 2020"
+    },
+    {
+		"content" : "Credit Card Fraud Detection",
+		"url": "https://github.com/suppy25/Credit-Card-Fraud-Detection",
+		"date": "July 12, 2020"
+    },
+    {
+		"content" : "Building-user-based-recommendation-model-for-Amazon",
+		"url": "https://github.com/suppy25/-Building-user-based-recommendation-model-for-Amazon",
+		"date": "July 19, 2020"
+    },
+    {
+		"content" : "INCOME-QUALIFICATION",
+		"url": "https://github.com/suppy25/INCOME-QUALIFICATION",
+		"date": "Aug 2, 2020"
+    }
+    
+	
+]
+
